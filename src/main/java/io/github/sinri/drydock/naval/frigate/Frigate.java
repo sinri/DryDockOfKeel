e188f9c951b01ca86daf3383994e0f20801d96a0
Sinri Edogawa <ljni@leqee.com>
2023-05-08 11:04:32 +0800

    1.0.1-SNAPSHOT Frigate
    Signed-off-by: Sinri Edogawa <ljni@leqee.com>

--- src/main/java/io/github/sinri/drydock/naval/frigate/Frigate.java
+++ src/main/java/io/github/sinri/drydock/naval/frigate/Frigate.java
@@ -3,6 +3,7 @@ package io.github.sinri.drydock.naval.frigate;
 import io.github.sinri.drydock.naval.caravel.AliyunSLSAdapterImpl;
 import io.github.sinri.drydock.naval.caravel.Caravel;
 import io.github.sinri.keel.servant.queue.KeelQueue;
+import io.github.sinri.keel.servant.queue.KeelQueueNextTaskSeeker;
 import io.github.sinri.keel.servant.sundial.KeelSundial;
 import io.github.sinri.keel.servant.sundial.KeelSundialPlan;
 import io.vertx.core.DeploymentOptions;
@@ -14,39 +15,81 @@ import java.util.Collection;
  * @since 1.0.1
  */
 public abstract class Frigate extends Caravel {
-    private final boolean queueEnabled;
-    private final boolean sundialEnabled;
-
-    public Frigate(boolean sundialEnabled, boolean queueEnabled) {
-        this.sundialEnabled = sundialEnabled;
-        this.queueEnabled = queueEnabled;
-    }
 
     @Override
     final protected void launchAsCaravel() {
-        if (queueEnabled) {
-            KeelQueue queue = this.buildQueue();
+        KeelQueue queue = this.buildQueue();
+        if (queue != null) {
             queue.setLogger(generateLogger(AliyunSLSAdapterImpl.TopicQueue, null));
             queue.deployMe(new DeploymentOptions().setWorker(true));
         }
-        if (sundialEnabled) {
-            KeelSundial sundial = new KeelSundial() {
-                @Override
-                protected Future<Collection<KeelSundialPlan>> fetchPlans() {
-                    return fetchSundialPlans();
-                }
-            };
-            sundial.setLogger(generateLogger(AliyunSLSAdapterImpl.TopicSundial, null));
+
+        KeelSundial sundial = this.buildSundial();
+        if (sundial != null) {
             sundial.deployMe(new DeploymentOptions().setWorker(true));
         }
     }
 
     abstract protected void launchAsFrigate();
 
+    /**
+     * @return an instance of KeelSundial, or null to turn off sundial
+     */
+    protected KeelSundial buildSundial() {
+        var sundial = new KeelSundial() {
+            @Override
+            protected Future<Collection<KeelSundialPlan>> fetchPlans() {
+                return fetchSundialPlans();
+            }
+        };
+        sundial.setLogger(generateLogger(AliyunSLSAdapterImpl.TopicSundial, null));
+        return sundial;
+    }
+
+    /**
+     * If sundial is turned on, it would be called to refresh sundial plans regularly.
+     *
+     * @return a future of: Collection of KeelSundialPlan, or null for NOT MODIFIED
+     */
     protected Future<Collection<KeelSundialPlan>> fetchSundialPlans() {
         // NOT MODIFIED
         return null;
     }
 
-    abstract protected KeelQueue buildQueue();
+    /**
+     * You must override buildQueueNextTaskSeeker and buildSignalReader if queue is turned on.
+     *
+     * @return an instance of KeelQueue, or null to turn off queue
+     */
+    protected KeelQueue buildQueue() {
+        var queue = new KeelQueue() {
+            @Override
+            protected KeelQueueNextTaskSeeker getNextTaskSeeker() {
+                return buildQueueNextTaskSeeker();
+            }
+
+            @Override
+            protected SignalReader getSignalReader() {
+                return buildSignalReader();
+            }
+        };
+        queue.setLogger(generateLogger(AliyunSLSAdapterImpl.TopicQueue, null));
+        return queue;
+    }
+
+    /**
+     * @return an instance of KeelQueueNextTaskSeeker
+     */
+    protected KeelQueueNextTaskSeeker buildQueueNextTaskSeeker() {
+        return () -> Future.failedFuture(new Exception("buildQueueNextTaskSeeker TO BE OVERRODE"));
+    }
+
+    protected KeelQueue.SignalReader buildSignalReader() {
+        return new KeelQueue.SignalReader() {
+            @Override
+            public Future<KeelQueue.QueueSignal> readSignal() {
+                return Future.failedFuture(new Exception("readSignal TO BE OVERRODE"));
+            }
+        };
+    }
 }
