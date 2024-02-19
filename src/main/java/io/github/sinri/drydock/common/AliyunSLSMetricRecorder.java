72ff59554dedabf669cd708fa51e8623b77044b9
Sinri Edogawa <ljni@leqee.com>
2024-02-19 16:30:34 +0800

    1.3.3 Adopt Keel 3.1.9 + Aliyun SLS Metric
    Signed-off-by: Sinri Edogawa <ljni@leqee.com>

---
+++ src/main/java/io/github/sinri/drydock/common/AliyunSLSMetricRecorder.java
@@ -0,0 +1,162 @@
+package io.github.sinri.drydock.common;
+
+import com.aliyun.openservices.aliyun.log.producer.LogProducer;
+import com.aliyun.openservices.aliyun.log.producer.Producer;
+import com.aliyun.openservices.aliyun.log.producer.ProducerConfig;
+import com.aliyun.openservices.aliyun.log.producer.ProjectConfig;
+import com.aliyun.openservices.log.common.LogItem;
+import io.github.sinri.keel.facade.KeelConfiguration;
+import io.github.sinri.keel.logger.event.center.KeelOutputEventLogCenter;
+import io.github.sinri.keel.logger.metric.KeelMetricRecord;
+import io.github.sinri.keel.logger.metric.KeelMetricRecorder;
+import io.vertx.core.Future;
+import io.vertx.core.Promise;
+
+import javax.annotation.Nullable;
+import java.util.*;
+
+import static io.github.sinri.keel.facade.KeelInstance.Keel;
+import static io.github.sinri.keel.helper.KeelHelpersInterface.KeelHelpers;
+
+public class AliyunSLSMetricRecorder extends KeelMetricRecorder {
+    public static final String TopicHealthMonitorMetric = "HealthMonitorMetric";
+
+    private static boolean disabled;
+    private final String project;
+    private final String logstore;
+    private final String source;
+    private final Producer producer;
+    private final String endpoint;
+
+    public AliyunSLSMetricRecorder() {
+        super();
+
+        KeelConfiguration aliyunSlsMetricConfig = Keel.getConfiguration().extract("aliyun", "sls_metric");
+        Objects.requireNonNull(aliyunSlsMetricConfig);
+
+        String disabledString = aliyunSlsMetricConfig.readString("disabled");
+        disabled = ("YES".equals(disabledString));
+
+        this.project = aliyunSlsMetricConfig.readString("project");
+        this.logstore = aliyunSlsMetricConfig.readString("logstore");
+        this.endpoint = aliyunSlsMetricConfig.readString("endpoint");
+        this.source = buildSource(aliyunSlsMetricConfig.readString("source"));
+
+        if (!disabled) {
+            String accessKeyId = aliyunSlsMetricConfig.readString("accessKeyId");
+            String accessKeySecret = aliyunSlsMetricConfig.readString("accessKeySecret");
+
+            producer = new LogProducer(new ProducerConfig());
+            Objects.requireNonNull(project);
+            Objects.requireNonNull(endpoint);
+            Objects.requireNonNull(accessKeyId);
+            Objects.requireNonNull(accessKeySecret);
+            producer.putProjectConfig(new ProjectConfig(project, endpoint, accessKeyId, accessKeySecret));
+
+            //KeelOutputEventLogCenter.getInstance().createLogger(getClass().getName()).info("Aliyun SLS Producer relied aliyunSlsConfig: " + aliyunSlsConfig.toJsonObject());
+        } else {
+            producer = null;
+        }
+    }
+
+    /**
+     * Build source from configuration.
+     * Source Expression should be:
+     * - EMPTY/BLANK STRING or NULL: use SLS default source generation;
+     * - A TEMPLATED STRING
+     * --- Rule 1: Replace [IP] to local address;
+     *
+     * @since 1.2.7 add source template and rule 1.
+     */
+    private static String buildSource(@Nullable String configuredSourceExpression) {
+        if (configuredSourceExpression == null || configuredSourceExpression.isBlank()) {
+            return "";
+        }
+        // Rule 1: Replace [IP] to local address
+        String localHostAddress = KeelHelpers.netHelper().getLocalHostAddress();
+        if (localHostAddress == null) {
+            Keel.getLogger().warning("Could not get local host address for SLS source!");
+            return "";
+        }
+        return configuredSourceExpression.replaceAll("\\[IP]", localHostAddress);
+    }
+
+    @Override
+    protected Future<Void> handleForTopic(String topic, List<KeelMetricRecord> list) {
+        if (list.isEmpty()) return Future.succeededFuture();
+
+//        System.out.println("dealWithLogsForOneTopic<"+topic+"> handling "+list.size()+" logs");
+
+        if (disabled) {
+            list.forEach(item -> {
+                KeelOutputEventLogCenter.getInstance().createLogger(getClass().getName()).log(log -> {
+                    log.reloadDataFromJsonObject(item.toJsonObject());
+                });
+            });
+            return Future.succeededFuture();
+        }
+
+        Promise<Void> promise = Promise.promise();
+        try {
+            List<LogItem> logItems = new ArrayList<>();
+            list.forEach(metricRecord -> {
+                logItems.add(buildLogItem(metricRecord));
+            });
+            producer.send(project, logstore, topic, source, logItems, result -> {
+                if (!result.isSuccessful()) {
+                    KeelOutputEventLogCenter.getInstance().createLogger(getClass().getName()).error(eventLog -> {
+                        eventLog.topic(getClass().getName()).message("Producer Send Error: " + result);
+                    });
+                }
+                promise.complete(null);
+            });
+        } catch (Throwable e) {
+            KeelOutputEventLogCenter.getInstance().createLogger(getClass().getName()).exception(e, "Aliyun SLS Producer Exception");
+            promise.fail(e);
+        }
+
+        return promise.future();
+    }
+
+    /**
+     * metricName: the metric name, eg: http_requests_count
+     * labels:     labels map, eg: {'idc': 'idc1', 'ip': '192.0.2.0', 'hostname': 'appserver1'}
+     * value:      double value, eg: 1.234
+     *
+     * @return LogItem
+     */
+    private LogItem buildLogItem(
+            KeelMetricRecord metricRecord
+            //        String metricName, Map<String, String> labels, double value
+    ) {
+        String labelsKey = "__labels__";
+        String timeKey = "__time_nano__";
+        String valueKey = "__value__";
+        String nameKey = "__name__";
+        LogItem logItem = new LogItem();
+        int timeInSec = (int) (metricRecord.timestamp() / 1000);
+        logItem.SetTime(timeInSec);
+        logItem.PushBack(timeKey, metricRecord.timestamp() + "000");
+        logItem.PushBack(nameKey, metricRecord.metricName());
+        logItem.PushBack(valueKey, String.valueOf(metricRecord.value()));
+
+        // 按照字典序对labels排序, 如果您的labels已排序, 请忽略此步骤。
+        metricRecord.label("source", this.source);
+        TreeMap<String, String> sortedLabels = new TreeMap<String, String>(metricRecord.labels());
+        StringBuilder labelsBuilder = new StringBuilder();
+
+        boolean hasPrev = false;
+        for (Map.Entry<String, String> entry : sortedLabels.entrySet()) {
+            if (hasPrev) {
+                labelsBuilder.append("|");
+            }
+            hasPrev = true;
+            labelsBuilder.append(entry.getKey());
+            labelsBuilder.append("#$#");
+            labelsBuilder.append(entry.getValue());
+        }
+        logItem.PushBack(labelsKey, labelsBuilder.toString());
+        return logItem;
+    }
+
+}
--- src/main/java/io/github/sinri/drydock/common/HealthMonitor.java
+++ src/main/java/io/github/sinri/drydock/common/HealthMonitor.java
@@ -1,16 +1,23 @@
 package io.github.sinri.drydock.common;
 
 import io.github.sinri.keel.helper.runtime.KeelRuntimeMonitor;
+import io.github.sinri.keel.logger.metric.KeelMetricRecord;
 import io.github.sinri.keel.verticles.KeelVerticleBase;
 import io.vertx.core.json.JsonObject;
 
-import javax.annotation.Nullable;
+import javax.annotation.Nonnull;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @since 1.0.0
  */
 public class HealthMonitor extends KeelVerticleBase {
     private final long startTimestamp;
+    protected boolean recordWithMetric = false;
+    protected boolean recordWithEvent = true;
+
+    private AliyunSLSMetricRecorder metricRecorder;
 
     public HealthMonitor() {
         startTimestamp = System.currentTimeMillis();
@@ -18,19 +25,27 @@ public class HealthMonitor extends KeelVerticleBase {
 
     /**
      * 如果需要初始化一些数据记录器等，就在这里。
+     * - modify the value of recordWithMetric and recordWithEvent;
+     * - initialize metricRecorder (but not start)
      */
     protected void prepare() {
 
     }
 
-    @Nullable
-    protected JsonObject moreEntriesForSnapshot() {
-        return null;
+    protected void moreMonitorItems(@Nonnull final JsonObject event, @Nonnull final List<HealthMonitorMetricRecord> metricRecords) {
+        // to be overridden
     }
 
     @Override
     public void start() {
         prepare();
+
+        if (recordWithMetric) {
+            if (metricRecorder == null) {
+                metricRecorder = new AliyunSLSMetricRecorder();
+            }
+            metricRecorder.start();
+        }
 
         // since 1.2.10 add a boot record
         getLogger().notice("BOOTED");
@@ -38,21 +53,144 @@ public class HealthMonitor extends KeelVerticleBase {
         new KeelRuntimeMonitor().startRuntimeMonitor(
                 60_000L,
                 monitorSnapshot -> {
-                    getLogger().info(eventLog -> {
-                        var snapshot = new JsonObject()
-                                .put("survived", System.currentTimeMillis() - startTimestamp)
-                                .put("gc", monitorSnapshot.getGCStat().toJsonObject())
-                                .put("cpu_time", monitorSnapshot.getCPUTime().toJsonObject())
-                                .put("hardware_memory", monitorSnapshot.getHardwareMemory().toJsonObject())
-                                .put("jvm_memory", monitorSnapshot.getJvmMemory().toJsonObject())
-                                .put("jvm_heap_memory", monitorSnapshot.getJvmHeapMemory().toJsonObject());
-                        JsonObject entries = moreEntriesForSnapshot();
-                        if (entries != null) {
-                            entries.forEach(entry -> snapshot.put(entry.getKey(), entry.getValue()));
-                        }
-                        eventLog.put("snapshot", snapshot);
-                    });
+                    long now = System.currentTimeMillis();
+
+                    JsonObject moreEvent = new JsonObject();
+                    List<HealthMonitorMetricRecord> moreMetricRecords = new ArrayList<>();
+                    moreMonitorItems(moreEvent, moreMetricRecords);
+
+                    if (recordWithMetric) {
+                        metricRecorder.recordMetric(
+                                HealthMonitorMetricRecord.asSurvived(
+                                                System.currentTimeMillis() - startTimestamp
+                                        )
+                                        .timestamp(now)
+                        );
+                        metricRecorder.recordMetric(
+                                HealthMonitorMetricRecord.asHardwareMemoryUsage(
+                                                1.0 * monitorSnapshot.getJvmMemoryResult().getPhysicalUsedBytes() / monitorSnapshot.getJvmMemoryResult().getPhysicalMaxBytes()
+                                        )
+                                        .timestamp(monitorSnapshot.getJvmMemoryResult().getStatTime())
+                        );
+                        metricRecorder.recordMetric(
+                                HealthMonitorMetricRecord.asJvmMemoryUsage(
+                                                1.0 * monitorSnapshot.getJvmMemoryResult().getRuntimeHeapUsedBytes() / monitorSnapshot.getJvmMemoryResult().getRuntimeHeapMaxBytes()
+                                        )
+                                        .timestamp(monitorSnapshot.getJvmMemoryResult().getStatTime())
+                        );
+                        metricRecorder.recordMetric(
+                                HealthMonitorMetricRecord.asJvmHeapMemoryUsedBytes(
+                                                monitorSnapshot.getJvmMemoryResult().getMxHeapUsedBytes()
+                                        )
+                                        .timestamp(monitorSnapshot.getJvmMemoryResult().getStatTime())
+
+                        );
+                        metricRecorder.recordMetric(
+                                HealthMonitorMetricRecord.asJvmNonHeapMemoryUsedBytes(
+                                                monitorSnapshot.getJvmMemoryResult().getMxNonHeapUsedBytes()
+                                        )
+                                        .timestamp(monitorSnapshot.getJvmMemoryResult().getStatTime())
+
+                        );
+                        metricRecorder.recordMetric(
+                                HealthMonitorMetricRecord.asCpuUsage(
+                                                monitorSnapshot.getCPUTime().getCpuUsage()
+                                        )
+                                        .timestamp(monitorSnapshot.getCPUTime().getStatTime())
+                        );
+                        metricRecorder.recordMetric(
+                                HealthMonitorMetricRecord.asMajorGCCount(
+                                                monitorSnapshot.getGCStat().getOldGCCount()
+                                        )
+                                        .timestamp(monitorSnapshot.getGCStat().getStatTime())
+                        );
+                        metricRecorder.recordMetric(
+                                HealthMonitorMetricRecord.asMajorGCTime(
+                                                monitorSnapshot.getGCStat().getOldGCTime()
+                                        )
+                                        .timestamp(monitorSnapshot.getGCStat().getStatTime())
+                        );
+                        metricRecorder.recordMetric(
+                                HealthMonitorMetricRecord.asMinorGCCount(
+                                                monitorSnapshot.getGCStat().getYoungGCCount()
+                                        )
+                                        .timestamp(monitorSnapshot.getGCStat().getStatTime())
+                        );
+                        metricRecorder.recordMetric(
+                                HealthMonitorMetricRecord.asMinorGCTime(
+                                                monitorSnapshot.getGCStat().getYoungGCTime()
+                                        )
+                                        .timestamp(monitorSnapshot.getGCStat().getStatTime())
+                        );
+                        moreMetricRecords.forEach(x -> {
+                            metricRecorder.recordMetric(x);
+                        });
+                    }
+
+                    if (recordWithEvent) {
+                        getLogger().info(eventLog -> {
+                            var snapshot = new JsonObject()
+                                    .put("survived", System.currentTimeMillis() - startTimestamp)
+                                    .put("gc", monitorSnapshot.getGCStat().toJsonObject())
+                                    .put("cpu_time", monitorSnapshot.getCPUTime().toJsonObject())
+                                    .put("jvm_memory_stat", monitorSnapshot.getJvmMemoryResult().toJsonObject())
+                                    // todo remove
+                                    .put("hardware_memory", monitorSnapshot.getHardwareMemory().toJsonObject())
+                                    .put("jvm_memory", monitorSnapshot.getJvmMemory().toJsonObject())
+                                    .put("jvm_heap_memory", monitorSnapshot.getJvmHeapMemory().toJsonObject());
+                            moreEvent.forEach(entry -> snapshot.put(entry.getKey(), entry.getValue()));
+                            eventLog.put("snapshot", snapshot);
+                        });
+                    }
                 }
         );
+    }
+
+    public static class HealthMonitorMetricRecord extends KeelMetricRecord {
+
+        public HealthMonitorMetricRecord(String metricName, double value) {
+            super(metricName, value);
+            this.topic(AliyunSLSMetricRecorder.TopicHealthMonitorMetric);
+        }
+
+        public static HealthMonitorMetricRecord asSurvived(long value) {
+            return new HealthMonitorMetricRecord("survived", value);
+        }
+
+        public static HealthMonitorMetricRecord asMinorGCCount(long value) {
+            return new HealthMonitorMetricRecord("minor_gc_count", value);
+        }
+
+        public static HealthMonitorMetricRecord asMinorGCTime(long value) {
+            return new HealthMonitorMetricRecord("minor_gc_time", value);
+        }
+
+        public static HealthMonitorMetricRecord asMajorGCCount(long value) {
+            return new HealthMonitorMetricRecord("major_gc_count", value);
+        }
+
+        public static HealthMonitorMetricRecord asMajorGCTime(long value) {
+            return new HealthMonitorMetricRecord("major_gc_time", value);
+        }
+
+        public static HealthMonitorMetricRecord asCpuUsage(double value) {
+            return new HealthMonitorMetricRecord("cpu_usage", value);
+        }
+
+        public static HealthMonitorMetricRecord asHardwareMemoryUsage(double value) {
+            return new HealthMonitorMetricRecord("hardware_memory_usage", value);
+        }
+
+        public static HealthMonitorMetricRecord asJvmMemoryUsage(double value) {
+            return new HealthMonitorMetricRecord("jvm_memory_usage", value);
+        }
+
+        public static HealthMonitorMetricRecord asJvmHeapMemoryUsedBytes(long value) {
+            return new HealthMonitorMetricRecord("jvm_heap_memory_used_bytes", value);
+        }
+
+        public static HealthMonitorMetricRecord asJvmNonHeapMemoryUsedBytes(long value) {
+            return new HealthMonitorMetricRecord("jvm_non_heap_memory_used_bytes", value);
+        }
     }
 }
