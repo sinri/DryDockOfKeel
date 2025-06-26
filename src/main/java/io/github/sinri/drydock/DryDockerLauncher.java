99612878359bc3409d0db4f80f323c1ac27f73a0
sinri <e.joshua.s.e@gmail.com>
2025-06-26 16:56:10 +0800

    Remove Vert.x Launcher dependency and legacy CLI handling
    - Removed Vert.x `Launcher` usage from `DryDockerLauncher`
    - Commented out `vertx-launcher-legacy-cli` dependency in `pom.xml`
    - Simplified `DryDockerLauncher` main method implementation

--- src/main/java/io/github/sinri/drydock/DryDockerLauncher.java
+++ src/main/java/io/github/sinri/drydock/DryDockerLauncher.java
@@ -1,9 +1,7 @@
 package io.github.sinri.drydock;
 
-import io.vertx.core.Launcher;
 
 public class DryDockerLauncher {
     public static void main(String[] args) {
-        Launcher.main(args);
     }
 }
